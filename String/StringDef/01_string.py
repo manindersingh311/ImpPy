d176302a0ad744302d36a51585e33ce32c26209c
Maninder Singh <manindersingh@192.168.1.52>
2025-10-16 17:03:04 +0530

    adding

---
+++ String/StringDef/01_string.py
@@ -0,0 +1,10 @@
+# Strings are immutable strings never change
+text = "this is a nice day"
+#this is a slicing of the string
+a = text[1:4] #if we want any type of change in string we add in variable
+a = text[1:] #know string is update  #output = his is a nice day
+a = text[:4] #know string is update #output = this
+a = text[-1] #output = y
+
+print(text) #this is a nice day
+print(a) #his
